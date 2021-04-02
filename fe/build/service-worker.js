034a26e37e552ab8ab599864b5acb20bfc205e39
naresh <agrawal.nar@northeastern.edu>
2021-04-02 15:54:10 -0400

    fix env

--- fe/build/service-worker.js
+++ fe/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a67b685d8a17ce2f5f7c38576dabb9dc.js"
+  "/precache-manifest.738a6067eb1871bbec9a2d5db7ba0581.js"
 );
 
 self.addEventListener('message', (event) => {
