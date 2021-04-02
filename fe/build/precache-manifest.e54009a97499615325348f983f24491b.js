19fa6ecb1fbd73173b7ec520d1eb957ae636cd39
naresh <agrawal.nar@northeastern.edu>
2021-04-02 16:48:31 -0400

    node modules

---
+++ fe/build/precache-manifest.e54009a97499615325348f983f24491b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b35715e3db6de5d1e7f9f83d985885b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c912f45ef178f3776e10",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "70b80edd643775e66359",
+    "url": "/static/js/2.4eb74f49.chunk.js"
+  },
+  {
+    "revision": "9a5d22d7b0e21afebd28f4768943f0bf",
+    "url": "/static/js/2.4eb74f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c912f45ef178f3776e10",
+    "url": "/static/js/main.dada6949.chunk.js"
+  },
+  {
+    "revision": "07ef167960929c3ab583",
+    "url": "/static/js/runtime-main.60388dba.js"
+  }
+]);
