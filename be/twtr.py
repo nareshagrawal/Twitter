266c7e9fbae084884fe2ff3e475ba442df2a17b6
naresh <agrawal.nar@northeastern.edu>
2021-04-03 14:43:10 -0400

    adding decode logic

--- be/twtr.py
+++ be/twtr.py
@@ -100,7 +100,8 @@ def encode_token(user_id, token_type):
     )
 
 def decode_token(token):
-    payload = jwt.decode(token, get_env_var("secret_key"))
+   print("secret_key", get_env_var("secret_key"))
+    payload = jwt.decode(token, get_env_var("secret_key"), algorithms=["HS256"])
     print("decode_token:", payload)
     return payload["sub"]
 
@@ -168,8 +169,8 @@ def login():
             access_token = encode_token(userid, "access")
             refresh_token = encode_token(userid, "refresh")
             response_object = {
-                "access_token": access_token.decode(),
-                "refresh_token": refresh_token.decode(),
+                "access_token": access_token,
+                "refresh_token": refresh_token,
             }
             #return response_object, 200
             #return response_object
@@ -211,7 +212,7 @@ def fastlogin():
                     # issue a new access token, keep the same refresh token
                     access_token = encode_token(userid, "access")
                     response_object = {
-                        "access_token": access_token.decode(),
+                        "access_token": access_token,
                         "refresh_token": refresh_token,
                     }
                     return jsonify((response_object, status.HTTP_200_OK))
